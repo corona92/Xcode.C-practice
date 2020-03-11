751d779a89a24e192ddcd3e2585d959d74cf0f6f
Dong-Hak Kim <dhkim@outlook.com>
2020-03-11 17:38:53 +0900

    Initial Commit

---
+++ Xcode.practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Xcode.practice
+//
+//  Created by Dong-Hak Kim on 2020/03/11.
+//  Copyright © 2020 Dong-Hak Kim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
