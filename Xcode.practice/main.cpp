128b8e65f017c77a84f60e5549531d346d457d47
Dong-Hak Kim <dhkim@outlook.com>
2020-03-12 15:23:33 +0900

    try to solve pro.6603-lotto

--- Xcode.practice/main.cpp
+++ Xcode.practice/main.cpp
@@ -5,10 +5,30 @@
 //  Created by Dong-Hak Kim on 2020/03/11.
 //  Copyright © 2020 Dong-Hak Kim. All rights reserved.
 //
+//6603. lotto
 
 #include<iostream>
+#include<vector>
 using namespace std;
 int main(){
-    cout << "this.is my first file."<<'\n';
+    int k;
+    vector <int> casetotal;
+    do{
+        cin >> k;
+        vector <int> s(k);
+        for(int i=0;i<k;i++){
+            cin >> s[i];
+        }
+        do{
+            // 순열 조합
+            for(int i=0;i<k;i++){
+                cout << s[i] << " ";
+            }
+            cout << '\n';
+        }while(next_permutation(s.begin(),s.end()));
+        
+    }while(k!=0);
+
+    cout << "success output!"<<'\n';
     return 0;
 }
